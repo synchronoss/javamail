50710ce9ec1f26d424bf92bf21e7da0ead8f990d
Chenthil kumar <chenthil.kumar@synchronoss.com>
2018-03-13 16:31:18 +0900

    MMSGW-661 | [CTC-13002] should error when IMAP return undefined TAG response

--- 1.5.2/src/main/java/com/sun/mail/imap/protocol/IMAPProtocol.java
+++ 1.5.2/src/main/java/com/sun/mail/imap/protocol/IMAPProtocol.java
@@ -203,7 +203,12 @@ public class IMAPProtocol extends Protocol {
 	// Check CAPABILITY
 	Response[] r = command("CAPABILITY", null);
 
-	handleResult(r[r.length-1]);
+	if (!r[r.length-1].isOK()) {
+	    if (r[r.length-1].isNO() || r[r.length-1].isBAD() || r[r.length-1].isBYE())
+		throw new ProtocolException(r[r.length-1].toString());
+	    else
+		throw new ParsingException(r[r.length-1]);
+	}
 
 	capabilities = new HashMap(10);
 	authmechs = new ArrayList(5);
